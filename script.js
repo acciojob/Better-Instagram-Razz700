f12d3520d16b567e24ff6f776ec11a2c53d52f99
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-26 16:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+let dragelem=null;
+        let temp=null;
+        let sw=null;
+        let a=document.getElementById("parent");
+         render();
+        function render(){
+          a=document.getElementById("parent");
+      for (let i = 0; i < a.children.length; i++) {
+        a.children[i].addEventListener("dragstart",(e)=>{ 
+          sw=e.target; 
+        dragelem=sw.cloneNode(true);
+        });
+        a.children[i].addEventListener("dragover",(e)=>{
+        e.preventDefault();
+    });}
+for (let k = 0; k < a.children.length; k++) {
+   a=document.getElementById("parent");
+    a.children[k].addEventListener("drop",(e)=>{
+      temp=e.target.cloneNode(true);
+      e.target.replaceWith(dragelem);
+     sw.replaceWith(temp);
+        dragelem=null;
+        render();
+    });
+  
+  }}
